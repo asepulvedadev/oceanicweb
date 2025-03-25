4fc4925a6024a190614dce523c0a24e3969a04f5
asepulvedadev <asepulvedadev@gmail.com>
2025-03-25 11:12:15 -0600

    Reconfiguración del proyecto para usar Bun y modo estático

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: netlify(),
+  output: 'static',
   integrations: [tailwind()],
   vite: {
     assetsInclude: ['**/*.svg']
